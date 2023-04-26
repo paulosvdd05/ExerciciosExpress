49c5f9f92d1db4f1c7fcceb6f46505adbf229e21
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-26 09:24:44 -0300

    retornando um array de objetos

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 
 app.get('/opa',(req, res) => {
-    res.json({
-        name: 'iPad 32Gb',
-        price: 1899.00,
-        discount: 0.12
-    })
+
+    res.json([
+        {id:7, name: 'ana', position:1},
+        {id:12, name: 'Carlos', position:2},
+        {id:17, name: 'Bia', position:3}
+    ])
+
+    // res.json({
+    //     name: 'iPad 32Gb',
+    //     price: 1899.00,
+    //     discount: 0.12
+    // })
+
     //res.send('<h1>Estou bem</h1><br><br><h2>Isto é HTML</h2>')
 })
 
