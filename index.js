67f46e2e3142b832cd868bbec9d711c280e0129a
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-04-26 09:21:21 -0300

    JSON na requisicao

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/opa',(req, res) => {
-    res.send('<h1>Estou bem</h1><br><br><h2>Isto é HTML</h2>')
+    res.json({
+        name: 'iPad 32Gb',
+        price: 1899.00,
+        discount: 0.12
+    })
+    //res.send('<h1>Estou bem</h1><br><br><h2>Isto é HTML</h2>')
 })
 
 app.listen(3000, () => {
